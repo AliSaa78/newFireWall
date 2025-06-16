a20d7a0e8acb427ab64ec94dbceeea0b9e632a73
Ali Saad <alibarmej@gmail.com>
2025-06-17 01:48:29 +0300

    update swagger routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(specs));
 app.use('/api', patientRoutes); 
 app.use('/api', invRouter); 
-app.use('/api/user', authRoutes); 
+app.use('/api', authRoutes); 
 app.use(PrescriptionRoutes);
 app.use(qrRoute);
 
